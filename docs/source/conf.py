9aecc7caf2556d315cd96e65f55175e69e5142fb
jungsohn <jung.jungrak.sohn@gmail.com>
2025-04-08 17:08:53 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'subseaBTS'
+copyright = '2025, Global Tomoc'
+author = 'Dr.Sohn'
 
 release = '0.1'
 version = '0.1.0'
